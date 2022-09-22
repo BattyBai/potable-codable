af5fea5c49c7ad43a6d109dac099b32b3f6d7be4
Isaac Pure <isaacpure@gmail.com>
2022-09-22 09:11:28 -0500

    making test files and cont learning Ruby

--- my_first_ruby_program.rb
+++ my_first_ruby_program.rb
@@ -1,17 +1,27 @@
-puts 'loading methods...'
+# puts 'loading methods...'
 
-#def-end blocks let us define methods
+# #def-end blocks let us define methods
 
-def my_first_method
-    [true, false].sample #final line of code in a ruby method is considered the 'return'
+# def my_first_method
+#     [true, false].sample #final line of code in a ruby method is considered the 'return'
+# end
+#  def my_second_method
+#     return 3
+
+#     my_first_method
+#  end
+
+#  def needs_params (param_one, param_two)
+#     [param_one, param_two].sample
+#  end
+# puts 'methods available...'
+
+# puts "I like big words" unless "potatoes".length > 2
+
+# puts "I'm not really enjoying Ruby" if "help".length >= 4
+
+x=0
+while x<=10 
+   puts x
+   x+=1
 end
- def my_second_method
-    return 3
-
-    my_first_method
- end
-
- def needs_params (param_one, param_two)
-    [param_one, param_two].sample
- end
-puts 'methods available...'
